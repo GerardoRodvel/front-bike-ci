23c09a02b7125bfa2a92afafe92c55afa99bdeb1
GerardoRodvel <jose_gerardo_lpz@hotmail.com>
2019-12-04 00:33:39 -0600

    Travis CI config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { CreateComponent } from './components/create/create.component';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes} from '@angular/router';
 import { BarraComponent } from './components/barra/barra.component';
-import { TallerComponent } from './components/taller/taller.component';
 import { TiendasComponent } from './components/tiendas/tiendas.component';
-import { EventosComponent } from './components/eventos/eventos.component';
+
 
 
 const routes: Routes = [
@@ -17,17 +15,10 @@ const routes: Routes = [
 
   },
   {
-    path: 'create',
-    component:CreateComponent
-  },
-  {
     path: 'home',
     component:HomeComponent
   },
   {
-    path: 'taller',
-    component:TallerComponent
-  },{
     path: 'tiendas',
     component:TiendasComponent
   }
@@ -37,12 +28,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    CreateComponent,
     HomeComponent,
     BarraComponent,
-    TallerComponent,
     TiendasComponent,
-    EventosComponent
   ],
   imports: [
     BrowserModule,
